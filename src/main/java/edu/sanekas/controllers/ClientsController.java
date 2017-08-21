a7ac07a990d940aa373ff80147e79df2ce3aa7e6
Alexander Sergeev <sanekas@yandex-team.ru>
2017-08-22 00:23:51 +0300

    swagger ui docs

--- src/main/java/edu/sanekas/controllers/ClientsController.java
+++ src/main/java/edu/sanekas/controllers/ClientsController.java
@@ -2,6 +2,8 @@ package edu.sanekas.controllers;
 
 import edu.sanekas.dao.ClientService;
 import edu.sanekas.model.Client;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,8 @@ import java.util.Collection;
  * Created by sanekas on 14/05/2017.
  */
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/")
+@Api(value = "clientbase", description = "REST API for collecting clients")
 public class ClientsController {
     private final ClientService clientService;
 
@@ -25,42 +28,51 @@ public class ClientsController {
         this.clientService = clientService;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "Get all clients")
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     public Collection<Client> getAll() {
         return clientService.getAll();
     }
 
-    @RequestMapping(params = "id", method = RequestMethod.GET)
+    @ApiOperation(value = "Get clients by id")
+    @RequestMapping(value = "/getById", params = "id", method = RequestMethod.GET)
     public Client getById(@RequestParam(name = "id") String id) {
         return clientService.getById(id);
     }
 
-    @RequestMapping(params = "name", method = RequestMethod.GET)
+    @ApiOperation(value = "Get clients by name")
+    @RequestMapping(value = "/getByName", params = "name", method = RequestMethod.GET)
     public Collection<Client> getByName(@RequestParam(name = "name") String name) {
         return clientService.getByName(name);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Create new client")
+    @RequestMapping(value = "/putNewClient", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void add(@RequestBody Client client) {
         clientService.add(client);
     }
 
+    @Deprecated
+    @ApiOperation(value = "Create new client with name")
     @RequestMapping(value = "/add", params = "name", method = RequestMethod.PUT)
     public void add(@RequestParam(name = "name") String name) {
         clientService.add(name);
     }
 
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Update existed client")
+    @RequestMapping(value = "/updateClient", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@RequestBody Client client) {
         clientService.update(client);
     }
 
-    @RequestMapping(params = "id", method = RequestMethod.DELETE)
+    @ApiOperation(value = "Delete client by id")
+    @RequestMapping(value = "/deleteById", params = "id", method = RequestMethod.DELETE)
     public void deleteById(@RequestParam(name = "id") String id) {
         clientService.deleteById(id);
     }
 
-    @RequestMapping(params = "name", method = RequestMethod.DELETE)
+    @ApiOperation(value = "Delete client by name")
+    @RequestMapping(value = "/deleteByName", params = "name", method = RequestMethod.DELETE)
     public void deleteByName(@RequestParam(name = "name") String name) {
         clientService.deleteByName(name);
     }
